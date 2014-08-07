e1d97b50799afa23114d258429f05e79be3a6696
FunkyMonkeyMonk <monkey@buildingbananas.com>
2014-08-07 19:36:14 -0400

    Fixing security group issue

--- tf/CoreOS.tf
+++ tf/CoreOS.tf
@@ -26,7 +26,7 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
-resource "aws_security_group" "allow_all" {
+resource "aws_security_group" "coreos-test" {
     name = "coreos-test"
     description = "Allow All inbound traffic on CoreOS Ports"
 
@@ -57,7 +57,7 @@ resource "aws_instance" "docker_host" {
   ami = "${lookup(var.aws_amis, var.aws_region)}"
   count = 3
   key_name = "${var.aws_key_name}"
-  security_groups = "coreos-test"
+  security_groups = ["${aws_security_group.coreos-test.name}"]
   user_data = "#cloud-config\n\ncoreos:\n  etcd:\n    discovery: ${var.token}\n    addr: $private_ipv4:4001\n    peer-addr: $private_ipv4:7001\n  units:\n    - name: etcd.service\n      command: start\n    - name: fleet.service\n      command: start"
 }
 
