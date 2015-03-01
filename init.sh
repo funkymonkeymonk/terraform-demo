1f754359a7286ec756690f54e3dd24163ad17933
funkymonkeymonk <monkey@buildingbananas.com>
2015-03-01 00:57:15 -0500

    Removing shipyard
    I've removed shipyard but I'm leaving the service in the service directory in
    hopes that a generic provisioning method can be used.

--- init.sh
+++ init.sh
@@ -58,12 +58,4 @@ do
     sleep 1
 done
 
-export FLEETCTL_ENDPOINT="http://$address:$port"
-echo "export FLEETCTL_ENDPOINT=\"http://$address:$port\""
-if ask "Would you like to reload shipyard?"; then
-    fleetctl destroy service/*
-    fleetctl load service/*
-    fleetctl start service/*
-fi
-
 
